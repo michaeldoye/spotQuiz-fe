63cdf38cdff1910b6c61f2d73b2e39ee907a5193
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-30 21:16:07 +0000

    Deploy michaeldoye/spotQuiz-fe to github.com/michaeldoye/spotQuiz-fe.git:gh-pages

---
+++ precache-manifest.dc55bfcd93176eefbaceee337b031e9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad20dcc18ed4a2fae3aaaa182fecfe3",
+    "url": "/spotQuiz-fe/index.html"
+  },
+  {
+    "revision": "c2539b75d01c8e0303df",
+    "url": "/spotQuiz-fe/static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "55e95ba5a7a72e2b6fb6",
+    "url": "/spotQuiz-fe/static/css/main.36bdf804.chunk.css"
+  },
+  {
+    "revision": "c2539b75d01c8e0303df",
+    "url": "/spotQuiz-fe/static/js/2.1965ad86.chunk.js"
+  },
+  {
+    "revision": "55e95ba5a7a72e2b6fb6",
+    "url": "/spotQuiz-fe/static/js/main.9d92b069.chunk.js"
+  },
+  {
+    "revision": "99a110dcc75933d4dc03",
+    "url": "/spotQuiz-fe/static/js/runtime-main.5fe8f048.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/spotQuiz-fe/static/media/logo.25bf045c.svg"
+  }
+]);
